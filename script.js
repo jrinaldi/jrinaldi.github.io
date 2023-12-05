380f24c77d4355a9caa7c5b3f2299f3fd806c37a
jrinaldi <joel.rinaldi@gmail.com>
2023-12-05 17:32:22 -0500

    Added color change

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
+
 let originalCount = 0;
 
 function updateCount() {
   const updatedCount = parseInt(document.getElementById('wordCountInput').value);
   const difference = updatedCount - originalCount;
-  
-  document.getElementById('difference').textContent = `Difference: ${difference}`;
+  const differenceText = document.getElementById('difference');
+
+  differenceText.textContent = `Difference: ${difference}`;
+
+  // Updating the color based on the difference value
+  if (difference < 500) {
+    differenceText.style.color = 'red';
+  } else if (difference >= 501 && difference <= 1500) {
+    differenceText.style.color = 'purple';
+  } else {
+    differenceText.style.color = 'green';
+  }
 }
 
 function saveCount() {
   originalCount = parseInt(document.getElementById('wordCountInput').value);
   document.getElementById('originalCount').textContent = originalCount;
   document.getElementById('difference').textContent = 'Difference: ';
+  document.getElementById('difference').style.color = ''; // Resetting color
 }
