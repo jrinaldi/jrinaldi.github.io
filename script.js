c4a09fdaa32ec6a82238e7236968b4f163a91f08
jrinaldi <joel.rinaldi@gmail.com>
2023-12-05 16:03:24 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+let originalCount = 0;
+
+function updateCount() {
+  const updatedCount = parseInt(document.getElementById('wordCountInput').value);
+  const difference = updatedCount - originalCount;
+  
+  document.getElementById('difference').textContent = `Difference: ${difference}`;
+}
+
+function saveCount() {
+  originalCount = parseInt(document.getElementById('wordCountInput').value);
+  document.getElementById('difference').textContent = 'Difference: ';
+}
